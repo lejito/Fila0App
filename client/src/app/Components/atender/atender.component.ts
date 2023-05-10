c54d87e480c9bf43a2db9f34328bf9b9c08ba294
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2023-05-10 14:19:01 -0500

    avance con atender

--- client/src/app/Components/atender/atender.component.ts
+++ client/src/app/Components/atender/atender.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Turno } from 'src/app/models/Turno';
+import { Usuario } from 'src/app/models/Usuario';
 
 @Component({
   selector: 'app-atender',
   templateUrl: './atender.component.html',
-  styleUrls: ['./atender.component.css']
+  styleUrls: ['./atender.component.css'],
 })
-export class AtenderComponent {
-
+export class AtenderComponent implements OnInit {
+  public turno: Turno = new Turno(
+    -1,
+    'AAA000',
+    '',
+    '2023-05-10T19:12:56.778Z',
+    0,
+    new Usuario(
+      -1,
+      'CC',
+      '1111111111',
+      'Pnombre',
+      'Snombre',
+      'Papellido',
+      'Sapellido'
+    )
+  );
+  ngOnInit(): void {}
 }

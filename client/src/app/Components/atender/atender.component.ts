347a8307c5f14bbcb07ee6beb7bcfcd8137734fb
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2023-05-10 15:23:21 -0500

    avance con funcionalidades de atender

--- client/src/app/Components/atender/atender.component.ts
+++ client/src/app/Components/atender/atender.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TurnoService } from 'src/app/Service/turno.service';
 import { Turno } from 'src/app/models/Turno';
 import { Usuario } from 'src/app/models/Usuario';
 
@@ -24,5 +25,52 @@ export class AtenderComponent implements OnInit {
       'Sapellido'
     )
   );
-  ngOnInit(): void {}
+  public categoria = 'N/A';
+  public TurnoCompletado = true;
+  public turnoEnCurso = false;
+  constructor(private turnoService: TurnoService) {}
+  ngOnInit(): void {
+    let turnoActual = localStorage.getItem('TURNO');
+    if (turnoActual) {
+      let turnoConvertido = JSON.parse(turnoActual);
+      this.turno = turnoConvertido;
+      this.turnoEnCurso = true;
+      this.TurnoCompletado = false;
+    }
+  }
+  siguiente() {
+    let modulo = sessionStorage.getItem('MODULO');
+    if (!modulo) {
+      return;
+    }
+    this.turnoService.asignar(modulo, this.categoria).subscribe((resp) => {
+      if (!resp.error) {
+        let usuario = new Usuario(
+          -1,
+          resp.tipoDocumento,
+          resp.numeroDocumento,
+          resp.primerNombre,
+          resp.segundoNombre,
+          resp.primerApellido,
+          resp.segundoApellido
+        );
+        let turno = new Turno(
+          resp.id,
+          resp.codigo,
+          '',
+          resp.fechaAsignado,
+          resp.modulo,
+          usuario
+        );
+        this.turno = turno;
+        this.TurnoCompletado = false;
+        this.turnoEnCurso = true;
+        let turnoStr = JSON.stringify(turno);
+        localStorage.setItem('TURNO', turnoStr);
+      }
+    });
+  }
+  completar() {
+    this.TurnoCompletado = true;
+  }
 }
